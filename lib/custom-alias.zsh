a3327bfd1226848c2f85d59e372ecfe2740c0041
Adel Hasic <hasic@student.chalmers.se>
2022-08-06 16:30:58 +0200

    Update lib to symlinks.

--- lib/custom-alias.zsh
+++ lib/custom-alias.zsh
@@ -1,8 +1,1 @@
-# Custom aliases for zsh
-# File: custom-alias.zsh
-# Path:
-#   With omz: automatically sourced from lib $ZSH/lib/custom-alias.zsh (~/.oh-my-zsh/lib/custom-alias.zsh) with the *.zsh suffix.
-#   Without omz: source from zshrc (or analogous startup config file).
-
-# Git aliases (complement to omz plugin 'git')
-alias gmnf='git merge --no-ff'
+/home/hasic/projects/linux-conf-kubuntu22/home-dir/.oh-my-zsh/lib/custom-alias.zsh
--- lib/custom-plugin-conf.zsh
+++ lib/custom-plugin-conf.zsh
@@ -1,10 +1,1 @@
-# Custom plugin configuration for zsh
-# File: custom-plugin-conf.zsh
-# Path:
-#   With omz: automatically sourced from lib $ZSH/lib/custom-plugin-conf.zsh (~/.oh-my-zsh/lib/custom-plugin-conf.zsh) with the *.zsh suffix.
-#   Without omz: source from zshrc (or analogous startup config file).
-
-### Plugin: ssh-agent
-# Do not load identities at startup (lazy).
-# Useful with ssh option AddKeysToAgent to enter password only on first use.
-zstyle :omz:plugins:ssh-agent lazy yes
+/home/hasic/projects/linux-conf-kubuntu22/home-dir/.oh-my-zsh/lib/custom-plugin-conf.zsh
